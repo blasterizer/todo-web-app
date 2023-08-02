bb5768e3447869e9353c7a5a73016186876e5507
blasterizer <blasterizer007@gmail.com>
2023-08-02 22:37:48 +0530

    Implemented Add todos feature in web app

--- ToDoApp/web.py
+++ ToDoApp/web.py
@@ -1,13 +1,20 @@
 import streamlit as st
 import functions
 
+todos = functions.get_todos()
+
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
 st.title("My Todo App")
 st.subheader("This is my ToDo App")
-st.write("This app is used to increaes Productivity")
-
-todos = functions.get_todos()
+st.write("This app is used to increase Productivity")
 
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="", placeholder="Add a new ToDo")
+st.text_input(label="", placeholder="Add a new ToDo", key="new_todo", on_change=add_todo)
