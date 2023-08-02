9ee913c3fe2c0ff15e0706e3d100667428945acb
blasterizer <blasterizer007@gmail.com>
2023-08-02 22:53:38 +0530

    Implemented the Complete feature in Web App

--- ToDoApp/web.py
+++ ToDoApp/web.py
@@ -8,13 +8,20 @@ def add_todo():
     todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
     functions.write_todos(todos)
+    st.session_state["new_todo"] = ""
 
 
 st.title("My Todo App")
 st.subheader("This is my ToDo App")
 st.write("This app is used to increase Productivity")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+
 
 st.text_input(label="", placeholder="Add a new ToDo", key="new_todo", on_change=add_todo)
